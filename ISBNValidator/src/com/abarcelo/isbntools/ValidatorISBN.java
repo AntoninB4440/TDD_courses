e671b0f3f05f0a9bc47850295e2892698cc8c40b
AntoninB_Apside <antonin.barcelo@apside-groupe.com>
2022-06-29 16:08:47 +0200

    Refactoring code after all test

--- ISBNValidator/src/com/abarcelo/isbntools/ValidatorISBN.java
+++ ISBNValidator/src/com/abarcelo/isbntools/ValidatorISBN.java
@@ -2,12 +2,46 @@ package com.abarcelo.isbntools;
 
 public class ValidatorISBN {
 
+    public static final int LONG_ISBN_LENGTH = 13;
+    public static final int SHORT_ISBN_LENGTH = 10;
+    public static final int SHORT_ISBN_MULTIPLIER = 11;
+    public static final int LONG_ISBN_MULTIPLIER = 10;
+
     public boolean checkISBN(String isbn){
+        if(isbn.length() == LONG_ISBN_LENGTH){
+            return isValidLongISBN(isbn);
+        } else if(isbn.length() == SHORT_ISBN_LENGTH){
+            return isValidShortISBN(isbn);
+        }
+        throw new NumberFormatException("ISBN numbers must be 10 or 13 digits long");
+    }
+
+    private boolean isValidShortISBN(String isbn) {
         int total = 0;
-        for(int i = 0;i < 10; i++){
-            total += isbn.charAt(i) * (10-i);
+        for(int i = 0;i < SHORT_ISBN_LENGTH; i++){
+            if(!Character.isDigit(isbn.charAt(i))){
+                if(i==9 && isbn.charAt(i) == 'X'){
+                    total += 10;
+                } else {
+                    throw new NumberFormatException("ISBN numbers can only contains numeric digit");
+                }
+            } else {
+                total += Character.getNumericValue(isbn.charAt(i)) * (SHORT_ISBN_LENGTH-i);
+            }
         }
 
-        return total % 11 == 0;
+        return total % SHORT_ISBN_MULTIPLIER == 0;
+    }
+
+    private boolean isValidLongISBN(String isbn) {
+        int total = 0;
+        for(int i = 0; i < LONG_ISBN_LENGTH; i++){
+            if(i% 2 == 0){
+                total += Character.getNumericValue(isbn.charAt(i));
+            } else {
+                total += Character.getNumericValue(isbn.charAt(i)) * 3;
+            }
+        }
+        return total % LONG_ISBN_MULTIPLIER == 0;
     }
 }
--- ISBNValidator/test/com/abarcelo/isbntools/ValidatorISBNTest.java
+++ ISBNValidator/test/com/abarcelo/isbntools/ValidatorISBNTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class ValidatorISBNTest {
 
     @Test
-    public void checkValidISBN(){
+    public void checkValid10DigitISBN(){
         ValidatorISBN validator = new ValidatorISBN();
         boolean result = validator.checkISBN("0140445927");
         assertTrue(result, "First value");
@@ -16,10 +16,46 @@ class ValidatorISBNTest {
     }
 
     @Test
-    public void checkAndInvalidISBN(){
+    public void checkValid13DigitISBN(){
+        ValidatorISBN validator = new ValidatorISBN();
+        boolean result = validator.checkISBN("9798838158192");
+        assertTrue(result, "First 13 ISBN digit value");
+        result = validator.checkISBN("9780140444445");
+        assertTrue(result, "Second 13 ISBN digit value");
+    }
+
+
+    @Test
+    public void checkAndInvalid10DigitISBN(){
         ValidatorISBN validator = new ValidatorISBN();
         boolean result = validator.checkISBN("0140445926");
         assertFalse(result);
+    }
+
+    @Test
+    public void checkAndInvalid13DigitISBN(){
+        ValidatorISBN validator = new ValidatorISBN();
+        boolean result = validator.checkISBN("9780140444448");
+        assertFalse(result);
+    }
+
+    @Test
+    public void TenDigitISBNNumberEndingWithAnXAreValid(){
+        ValidatorISBN validator = new ValidatorISBN();
+        boolean result = validator.checkISBN("012000030X");
+        assertTrue(result, "ISBN ending with X");
+    }
+
+    @Test()
+    public void nineDigitISBNsAreNotAllowed(){
+        ValidatorISBN validator = new ValidatorISBN();
+        assertThrows(NumberFormatException.class,() ->  validator.checkISBN("123456789"));
+    }
+
+    @Test()
+    public void nonStringNumberISBNsAreNotAllowed(){
+        ValidatorISBN validator = new ValidatorISBN();
+        assertThrows(NumberFormatException.class,() ->  validator.checkISBN("helloworld"));
     }
 
 }
