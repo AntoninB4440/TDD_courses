c8fbd9442096719595df7c7496a6d44be1fa6a55
AntoninB_Apside <antonin.barcelo@apside-groupe.com>
2022-07-04 15:37:48 +0200

    Test implementation with mock and verify method to check behavior

--- ISBNValidator/src/com/abarcelo/isbntools/StockManager.java
+++ ISBNValidator/src/com/abarcelo/isbntools/StockManager.java
@@ -2,13 +2,16 @@ package com.abarcelo.isbntools;
 
 public class StockManager {
 
-    private final ExternalISBNDataService externalISBNDataService;
-    public StockManager(ExternalISBNDataService externalISBNDataService) {
-        this.externalISBNDataService = externalISBNDataService;
+    private final ExternalISBNDataService webService;
+    private final ExternalISBNDataService databaseService;
+    public StockManager(ExternalISBNDataService webService, ExternalISBNDataService databaseService) {
+        this.webService = webService;
+        this.databaseService = databaseService;
     }
 
     public String getLocatorCode(String isbn) {
-        Book book = externalISBNDataService.lookup(isbn);
+        Book book = databaseService.lookup(isbn);
+        if (book == null) book = webService.lookup(isbn);
         StringBuilder locator = new StringBuilder();
         locator.append(isbn.substring(isbn.length() - 4))
                 .append(book.getAuthor().charAt(0))
--- ISBNValidator/test/com/abarcelo/isbntools/StockManagerTest.java
+++ ISBNValidator/test/com/abarcelo/isbntools/StockManagerTest.java
@@ -3,6 +3,7 @@ package com.abarcelo.isbntools;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 public class StockManagerTest {
 
@@ -15,11 +16,46 @@ public class StockManagerTest {
             }
         };*/
 
-        ExternalISBNDataService externalISBNDataService = isbn -> new Book(isbn,"The Iliad","Homer");
-
+        ExternalISBNDataService webService = isbn -> new Book(isbn,"The Iliad","Homer");
+        ExternalISBNDataService databaseService = isbn -> null;
         String isbn = "0140445927"; //The Iliad Book by Homer
-        StockManager stockmanager = new StockManager(externalISBNDataService);
+        StockManager stockmanager = new StockManager(webService, databaseService);
         String locatorCode = stockmanager.getLocatorCode(isbn);
         assertEquals("5927H2", locatorCode);
+    }
+
+    @Test
+    public void databaseIsUsedIfDataIsPresent(){
+        ExternalISBNDataService databaseService = mock(ExternalISBNDataService.class);
+        ExternalISBNDataService webService = mock(ExternalISBNDataService.class);
+
+        when(databaseService.lookup("0140445927"))
+                .thenReturn(new Book("0140445927","ABC","ABC"));
+
+        String isbn = "0140445927"; //The Iliad Book by Homer
+        StockManager stockmanager = new StockManager(webService, databaseService);
+        String locatorCode = stockmanager.getLocatorCode(isbn);
+
+        verify(databaseService).lookup("0140445927");
+        verify(webService, never()).lookup(anyString());
+    }
+
+    @Test
+    public void webServiceIsUsedIfDataIsNotPresentInDB(){
+        ExternalISBNDataService databaseService = mock(ExternalISBNDataService.class);
+        ExternalISBNDataService webService = mock(ExternalISBNDataService.class);
+
+        when(databaseService.lookup("0140445927"))
+                .thenReturn(null);
+
+        when(webService.lookup("0140445927"))
+                .thenReturn(new Book("0140445927","ABC","ABC"));
+
+        String isbn = "0140445927"; //The Iliad Book by Homer
+        StockManager stockmanager = new StockManager(webService, databaseService);
+        String locatorCode = stockmanager.getLocatorCode(isbn);
+
+        verify(databaseService).lookup("0140445927");
+        verify(webService, times(1)).lookup("0140445927");
     }
 }
